4f64eb5b9e897ff01d749b198d389e54c5ae8ece
Charles Pereira <charlesti.ptu@gmail.com>
2019-12-19 16:15:21 -0300

    permissão para usuários sql server

--- SQL/Query - Alteração de usuário e schema do cadastro de logon.sql
+++ SQL/Query - Alteração de usuário e schema do cadastro de logon.sql
@@ -8,3 +8,21 @@ ALTER USER uca WITH NAME = uca1;
 ALTER USER uca1 WITH DEFAULT_SCHEMA = uca1;  
 GO 
 
+
+/*PERMISSÃO A USUÁRIOS. OBS.: TEM QUE SER EXECUTADO NO MASTER*/
+
+use DBUG
+go
+GRANT execute,select,insert,update,delete TO UUG
+
+use DBCA
+go
+GRANT execute,select,insert,update,delete TO UCA
+
+use DBPR
+go
+GRANT execute,select,insert,update,delete TO UPR
+
+use DBAV
+go
+GRANT execute,select,insert,update,delete TO uav
